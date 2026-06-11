85cc7adc1663996a53f01d0fae2446a23a22bd05
temo3d <ewandooonly@gmail.com>
2026-06-11 08:05:53 -0300

    feat: add function definition exercise

---
+++ 01-definition.js
@@ -0,0 +1,11 @@
+/*const showGreeting = () => {
+    const text = 'Hello, Hexlet!';
+    console.log(text);
+}
+showGreeting();*/
+
+const printMotto = () => {
+    const text = 'Winter is coming';
+    console.log(text)
+}
+printMotto();
