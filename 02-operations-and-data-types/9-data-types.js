fcd279888a5c2c049b8ae4cf3a1e76f887120596
temo3d <ewandooonly@gmail.com>
2026-07-13 22:53:34 -0300

    feat: solve 16-data-types

---
+++ 02-operations-and-data-types/9-data-types.js
@@ -0,0 +1,6 @@
+const currentYear = 2026;
+const birthyear = 1994;
+console.log('Name: Anna');
+console.log('Birth year: ' + birthyear);
+console.log('Age: ' + (currentYear - birthyear));
+console.log('Rating: 4.7');
